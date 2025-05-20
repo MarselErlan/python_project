84f700ee633c29fa39dafea8ed1ff5cc71add99c
AbduraimovErlan <96403251+AbduraimovErlan@users.noreply.github.com>
2025-05-20 09:17:09 -0500

    05.20.25

---
+++ pythonMySQL/MySQLCreateDatabase.py
@@ -0,0 +1,36 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("CREATE DATABASE mydatabase")
+
+
+
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="yourusername",
+  password="yourpassword"
+)
+
+mycursor = mydb.cursor()
+
+mycursor.execute("SHOW DATABASES")
+
+for x in mycursor:
+  print(x)
+
+
+
+  mydb = mysql.connector.connect(
+      host="localhost",
+      user="yourusername",
+      password="yourpassword",
+      database="mydatabase"
+  )
